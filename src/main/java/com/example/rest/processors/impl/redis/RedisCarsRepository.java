7d72e4eb3d80b5da81430de1af2dd0ad1910e105
Kastalski Sergey <kasergey87@gmail.com>
2018-05-06 18:58:18 +0300

    Deleted RedisCarsRepository

--- src/main/java/com/example/rest/processors/impl/redis/RedisCarsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rest.processors.impl.redis;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.rest.domain.Car;
-
-public interface RedisCarsRepository extends CrudRepository<Car, Integer> {
-	
-}
