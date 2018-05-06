c4c7ec3e53cbf2a1ce39c04a9af5f717f029632e
Kastalski Sergey <kasergey87@gmail.com>
2018-05-06 18:36:57 +0300

    Refactoring.

--- src/main/java/com/example/rest/dao/impl/CarsDAOWithCacheImpl.java
+++ src/main/java/com/example/rest/dao/impl/CarsDAOWithCacheImpl.java
@@ -1,42 +1,22 @@
 package com.example.rest.dao.impl;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import com.example.rest.dao.CarsDAO;
-import com.example.rest.dao.JdbcExt;
-import com.example.rest.dao.impl.helpers.CarsRowMapper;
 import com.example.rest.domain.Car;
 
 @Repository("rest.withCache.carsDAO")
-public class CarsDAOWithCacheImpl extends JdbcExt implements CarsDAO {
-	
-	private static final String SELECT_CARS = "select id, name from cars";
-	
-	private static final String SELECT_CAR = "select id, name from cars where id=:id";
-	
-	private static final String CREATE_CAR = "insert into cars (name) values (:name)";
-	
-	private static final String UPDATE_CAR = "update cars set name=:name where id=:id";
-	
-	private static final String DELETE_CAR = "delete from cars where id=:id";
+public class CarsDAOWithCacheImpl extends CarsDAOImpl {
 	
 	@Override
 	@Cacheable(value="carsCacheList", sync=true, cacheManager="cacheManager")
 	public List<Car> getCars() {
-		System.out.println("get Cars from DB");
-		return getJdbcTemplate().query(SELECT_CARS, new CarsRowMapper());
+		return super.getCars();
 	}
 	
 	@Override
@@ -46,30 +26,13 @@ public class CarsDAOWithCacheImpl extends JdbcExt implements CarsDAO {
 	@Override
 	@Cacheable(value="carsCache", key="#id", cacheManager="cacheManager")
 	public Car getCar(final Integer id) {
-		System.out.println("get Car from DB by id " + id);
-		final Map<String, Object> params = new HashMap<>();
-		params.put("id", id);
-		try {
-			return getNamedParamJdbcTemplate().queryForObject(SELECT_CAR, params, new CarsRowMapper());
-		} catch (EmptyResultDataAccessException ex) {
-			return null;
-		}
+		return super.getCar(id);
 	}
 	
 	@Override
 	@CachePut(value="carsCache", key="#result.getId()", unless="#result == null", cacheManager="cacheManager")
 	public Car createCar(final Car car) {
-		final MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("name", car.getName());
-		
-		final KeyHolder keyHolder = new GeneratedKeyHolder();
-		if (getNamedParamJdbcTemplate().update(CREATE_CAR, params, keyHolder) > 0) {
-			car.setId(keyHolder.getKey().intValue());
-			System.out.println("created Car with id " + car.getId());
-			return car;
-		}
-		System.out.println("Not created Car");
-		return null;
+		return super.createCar(car);
 	}
 	
 	@Override
@@ -78,16 +41,7 @@ public class CarsDAOWithCacheImpl extends JdbcExt implements CarsDAO {
 		put = { @CachePut(value="carsCache", key="#car.getId()", unless="#result == null", cacheManager="cacheManager") }
 	)
 	public Car updateCar(final Car car) {
-		final Map<String, Object> params = new HashMap<>();
-		params.put("id", car.getId());
-		params.put("name", car.getName());
-		
-		if (getNamedParamJdbcTemplate().update(UPDATE_CAR, params) > 0) {
-			System.out.println("updated Car with id " + car.getId());
-			return car;
-		}
-		System.out.println("Not updated Car");
-		return null;
+		return super.updateCar(car);
 	}
 	
 	@Override
@@ -98,15 +52,7 @@ public class CarsDAOWithCacheImpl extends JdbcExt implements CarsDAO {
 		}
 	)
 	public boolean deleteCar(final Integer id) {
-		final Map<String, Object> params = new HashMap<>();
-		params.put("id", id);
-		
-		if (getNamedParamJdbcTemplate().update(DELETE_CAR, params) > 0) {
-			System.out.println("deleted Car with id " + id);
-			return true;
-		}
-		System.out.println("Not deleted Car");
-		return false;
+		return super.deleteCar(id);
 	}
 	
 }
--- src/main/java/com/example/rest/processors/impl/RedisCarsProcessor.java
+++ src/main/java/com/example/rest/processors/impl/RedisCarsProcessor.java
@@ -1,124 +1,15 @@
 package com.example.rest.processors.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import com.example.rest.dao.CarsDAO;
-import com.example.rest.domain.Car;
-import com.example.rest.domain.Cars;
-import com.example.rest.processors.impl.redis.RedisCarsRepository;
 
 @Service("service.carsRedisProcessor")
 public class RedisCarsProcessor extends AbstractCarsProcessor {
 	
-	@Autowired
-	private RedisCarsRepository redisCarsRepo;
-	
-	public RedisCarsProcessor(@Qualifier("rest.carsDAO") CarsDAO carsDAO) {
+	public RedisCarsProcessor(@Qualifier("rest.withRedisCache.carsDAO") CarsDAO carsDAO) {
 		super(carsDAO);
-	}
-	
-	@Override
-	public ResponseEntity<Cars> processGetCars() {
-		final List<Car> carsList;
-		try {
-			synchronized (redisCarsRepo) {
-				if (redisCarsRepo.count() > 0) {
-					carsList = new ArrayList<>();
-					redisCarsRepo.findAll().forEach(carsList::add);
-				} else {
-					carsList = carsDAO.getCars();
-					if (!CollectionUtils.isEmpty(carsList)) {
-						redisCarsRepo.save(carsList);
-					}
-				}
-			}
-			
-			return ResponseEntity.ok().body(new Cars(carsList));
-		} catch (Exception ex) {
-			processException(ex);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	@Override
-	public ResponseEntity<Car> processGetCar(final int id) {
-		try {
-			Car car;
-			synchronized (redisCarsRepo) {
-				car = redisCarsRepo.findOne(id);
-				if (car == null) {
-					car = carsDAO.getCar(id);
-					if (car != null) {
-						redisCarsRepo.save(car);
-					}
-				}
-			}
-			
-			if (car == null) {
-				return ResponseEntity.badRequest().build();
-			}
-			return ResponseEntity.ok().body(car);
-		} catch (Exception ex) {
-			processException(ex);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	@Override
-	public ResponseEntity<Car> processCreateCar(final Car car) {
-		try {
-			final Car newCar = carsDAO.createCar(car);
-			if (newCar != null) {
-				synchronized (redisCarsRepo) {
-					redisCarsRepo.save(newCar);
-				}
-				return ResponseEntity.status(HttpStatus.CREATED).body(newCar);
-			}
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		} catch (Exception ex) {
-			processException(ex);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	@Override
-	public ResponseEntity<?> processUpdateCar(final Car car) {
-		try {
-			if (carsDAO.updateCar(car) != null) {
-				synchronized (redisCarsRepo) {
-					redisCarsRepo.save(car);
-				}
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-			}
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		} catch (Exception ex) {
-			processException(ex);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	@Override
-	public ResponseEntity<?> processDeleteCar(final int id) {
-		try {
-			if (carsDAO.deleteCar(id)) {
-				synchronized (redisCarsRepo) {
-					redisCarsRepo.delete(id);
-				}
-				return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-			}
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-		} catch (Exception ex) {
-			processException(ex);
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
 	}
 	
 }
